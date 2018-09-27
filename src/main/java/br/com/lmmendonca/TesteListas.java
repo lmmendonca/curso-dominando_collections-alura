bc15dc7293c12e58436977d891a6e9d112a36448
lmaximino <lmaximino.mendonca@gmail.com>
2018-09-27 18:14:55 -0300

    Commit inicial

---
+++ src/main/java/br/com/lmmendonca/TesteListas.java
@@ -0,0 +1,24 @@
+package br.com.lmmendonca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteListas {
+
+    public static void main(String[] args) {
+
+        String aula1 = "Conhecendo mais de aulas.";
+        String aula2 = "Modelando a classe Aula";
+        String aula3 = "Trabalhando com Cursos e Sets";
+
+        List<String> aulas = new ArrayList<String>();
+
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+
+
+
+    }
+}
