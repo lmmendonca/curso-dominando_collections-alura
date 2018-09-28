97bf4d9cf8727ef833f14444a41fa6c93bb6d24c
lmaximino <lmaximino.mendonca@gmail.com>
2018-09-28 16:59:27 -0300

    Collections.emptySet()

---
+++ src/main/java/br/com/lmmendonca/TesteEmptySet.java
@@ -0,0 +1,15 @@
+package br.com.lmmendonca;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class TesteEmptySet {
+
+    public static void main(String[] args) {
+//        Interessante para utilizar quando se tenta cadastrar algum aluno a um curso desativado.
+        Set<String> nomes = Collections.emptySet();
+        nomes.add("Leonardo");
+
+        System.out.println(nomes);
+    }
+}
